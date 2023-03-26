14e2a221a9534b42fdfd376a35de7258a9fd2ab7
あきら <akirakirakira0139@gmail.com>
2023-03-26 02:12:00 +0000

    top page

---
+++ resources/views/commons/link_items.blade.php
@@ -0,0 +1,15 @@
+@if (Auth::check())
+    {{-- マイページへのリンク --}}
+    <li><a class="link link-hover" href="#">my page</a></li>
+    <li class="divider lg:hidden"></li>
+    {{-- 投稿ページへのリンク --}}
+    <li><a class="link link-hover" href="#">投稿</a></li>
+    {{-- ログアウトへのリンク --}}
+    <li><a class="link link-hover" href="#" onclick="event.preventDefault();this.closest('form').submit();">Logout</a></li>
+@else
+    {{-- ユーザ登録ページへのリンク --}}
+    <li><a class="link link-hover" href="{{ route('register') }}">Signup</a></li>
+    <li class="divider lg:hidden"></li>
+    {{-- ログインページへのリンク --}}
+    <li><a class="link link-hover" href="{{ route('login') }}">Login</a></li>
+@endif
